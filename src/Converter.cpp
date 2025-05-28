aec92973c9f42be50ff9efe9c7ea58b187b5cb1d
AEndrix <redegalli.andrea03@gmail.com>
2025-05-28 02:21:35 +0200

    enhance convertion logs

--- src/Converter.cpp
+++ src/Converter.cpp
@@ -20,15 +20,23 @@ namespace stl2glb {
         std::string stl_path = "/tmp/" + stl_hash + ".stl";
         std::string glb_path = "/tmp/" + stl_hash + ".glb";
 
+        Logger::info("Start downloading STL file with hash: " + stl_hash);
         MinioClient::download(env.getStlBucketName(), stl_hash, stl_path);
+        Logger::info("STL file downloaded");
 
+        Logger::info("STL Parsing...");
         auto triangles = STLParser::parse(stl_path);
+        Logger::info("STL Parsed! GLB writing...");
         GLBWriter::write(triangles, glb_path);
+        Logger::info("GLB write!");
 
+        Logger::info("Calculating hash of converted file");
         std::string glb_hash = Hasher::sha256_file(glb_path);
+        Logger::info("Hash: " + glb_hash);
 
         std::string final_glb = "/tmp/" + glb_hash + ".glb";
         fs::rename(glb_path, final_glb);
+        Logger::info("Uploading converted file to bucket...");
         MinioClient::upload(env.getGlbBucketName(), glb_hash, final_glb);
 
         Logger::info("Completed conversion: GLB hash = " + glb_hash);
