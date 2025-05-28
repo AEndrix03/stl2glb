3f2a2ced934e2f80a5f80980b688541f891ae173
AEndrix <redegalli.andrea03@gmail.com>
2025-05-28 02:09:33 +0200

    logging test

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,8 +1,10 @@
 #include "stl2glb/Server.hpp"
 #include "stl2glb/Converter.hpp"
+#include "stl2glb/Logger.hpp"
 #include <httplib.h>
 
 #include <nlohmann/json.hpp>
+#include <iostream>
 
 using json = nlohmann::json;
 
@@ -11,7 +13,10 @@ namespace stl2glb {
     void Server::start(int port) {
         httplib::Server svr;
 
+        std::cout << "[stl2glb] Server started on port " << port << std::endl;
+
         svr.Post("/convert", [](const httplib::Request& req, httplib::Response& res) {
+            stl2glb::Logger::info("Received /convert POST request");
             try {
                 auto body = json::parse(req.body);
                 std::string stl_hash = body.at("stl_hash");
@@ -20,10 +25,12 @@ namespace stl2glb {
 
                 res.set_content(json{{"glb_hash", glb_hash}}.dump(), "application/json");
             } catch (const std::exception& e) {
+                stl2glb::Logger::error(std::string("Error in /convert: ") + e.what());
                 res.status = 400;
                 res.set_content(json{{"error", e.what()}}.dump(), "application/json");
             }
         });
+
 
         std::cout << "[stl2glb] Server listening on port " << port << std::endl;
         svr.listen("0.0.0.0", port);
